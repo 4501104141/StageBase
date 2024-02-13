b2d2ebe8575317ca71eddfe06016f32ec7ea292c
Minh <4501104141@student.hcmue.edu.vn>
2024-02-13 20:36:46 +0700

    seeding_data

--- src/KnowledgeSpace.BackendServer/Program.cs
+++ src/KnowledgeSpace.BackendServer/Program.cs
@@ -1,5 +1,10 @@
+using KnowledgeSpace.BackendServer.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
 
 namespace KnowledgeSpace.BackendServer
 {
@@ -7,14 +12,35 @@ namespace KnowledgeSpace.BackendServer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                             .Enrich.FromLogContext()
+                             .WriteTo.Console()
+                             .CreateLogger();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    Log.Information("Seeding data...");
+                    var dbInitializer = services.GetService<DbInitializer>();
+                    dbInitializer.Seed().Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .UseSerilog()
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
     }
 }

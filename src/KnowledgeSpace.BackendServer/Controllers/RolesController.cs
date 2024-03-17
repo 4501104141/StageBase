b3a89aae5e7efe197ac2629ca4bb1f6f13944e91
Minh <4501104141@student.hcmue.edu.vn>
2024-03-17 21:42:42 +0700

    api user

--- src/KnowledgeSpace.BackendServer/Controllers/RolesController.cs
+++ src/KnowledgeSpace.BackendServer/Controllers/RolesController.cs
@@ -9,10 +9,7 @@ using System.Threading.Tasks;
 
 namespace KnowledgeSpace.BackendServer.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize("Bearer")]
-    public class RolesController : ControllerBase
+    public class RolesController : BaseController
     {
         private readonly RoleManager<IdentityRole> _roleManager;
 
@@ -23,18 +20,18 @@ namespace KnowledgeSpace.BackendServer.Controllers
 
         //URL: POST: http://localhost:5001/api/roles
         [HttpPost]
-        public async Task<IActionResult> PostRole(RoleVm roleVm)
+        public async Task<IActionResult> PostRole(RoleCreateRequest request)
         {
             var role = new IdentityRole()
             {
-                Id = roleVm.Id,
-                Name = roleVm.Name,
-                NormalizedName = roleVm.Name.ToUpper()
+                Id = request.Id,
+                Name = request.Name,
+                NormalizedName = request.Name.ToUpper()
             };
             var result = await _roleManager.CreateAsync(role);
             if (result.Succeeded)
             {
-                return CreatedAtAction(nameof(GetById), new { id = role.Id }, roleVm);
+                return CreatedAtAction(nameof(GetById), new { id = role.Id }, request);
             }
             else
             {
@@ -100,9 +97,9 @@ namespace KnowledgeSpace.BackendServer.Controllers
 
         //URL: PUT: http://localhost:5001/api/roles/{id}
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutRole(string id, [FromBody] RoleVm roleVm)
+        public async Task<IActionResult> PutRole(string id, [FromBody] RoleCreateRequest request)
         {
-            if (id != roleVm.Id)
+            if (id != request.Id)
             {
                 return BadRequest();
             }
@@ -111,8 +108,8 @@ namespace KnowledgeSpace.BackendServer.Controllers
             {
                 return NotFound();
             }
-            role.Name = roleVm.Name;
-            role.NormalizedName = roleVm.Name.ToUpper();
+            role.Name = request.Name;
+            role.NormalizedName = request.Name.ToUpper();
             var result = await _roleManager.UpdateAsync(role);
             if (result.Succeeded)
             {
---
+++ src/KnowledgeSpace.BackendServer/Controllers/UsersController.cs
@@ -0,0 +1,161 @@
+﻿using KnowledgeSpace.ViewModels.Systems;
+using KnowledgeSpace.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using KnowledgeSpace.BackendServer.Data.Entities;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userUser)
+        {
+            _userManager = userUser;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUser(UserCreateRequest request)
+        {
+            var user = new User()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Dob = request.Dob
+            };
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (result.Succeeded)
+            {
+                return CreatedAtAction(nameof(GetById), new { id = user.Id }, request);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = _userManager.Users;
+            var uservms = await users.Select(u => new UserVm()
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                Dob = u.Dob,
+                Email = u.Email,
+                PhoneNumber = u.PhoneNumber,
+                FirstName = u.FirstName,
+                LastName = u.LastName
+            }).ToListAsync();
+            return Ok(uservms);
+        }
+
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetUsersPaging(string filter, int pageIndex, int pageSize)
+        {
+            var query = _userManager.Users;
+            if (!string.IsNullOrEmpty(filter))
+            {
+                query = query.Where(x => x.Email.Contains(filter)
+                || x.UserName.Contains(filter)
+                || x.PhoneNumber.Contains(filter));
+            }
+            var totalRecords = await query.CountAsync();
+            var items = await query.Skip((pageIndex - 1 * pageSize))
+                .Take(pageSize)
+                .Select(u => new UserVm()
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    Dob = u.Dob,
+                    Email = u.Email,
+                    PhoneNumber = u.PhoneNumber,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName
+                })
+                .ToListAsync();
+            var pagination = new Pagination<UserVm>
+            {
+                Items = items,
+                TotalRecords = totalRecords,
+            };
+            return Ok(pagination);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var userVm = new UserVm()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Dob = user.Dob,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName
+            };
+            return Ok(userVm);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(string id, [FromBody] UserCreateRequest request)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if(user==null)
+            {
+                return NotFound();
+            }
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.Dob = request.Dob;
+            var result = await _userManager.UpdateAsync(user);
+            if(result.Succeeded)
+            {
+                return NoContent();
+            }
+            return BadRequest(result.Errors);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                var uservm = new UserVm()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Dob = user.Dob,
+                    Email = user.Email,
+                    PhoneNumber = user.PhoneNumber,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName
+                };
+                return Ok(uservm);
+            }
+            return BadRequest(result.Errors);
+        }
+    }
+}

02a35890a58b4742afaad156c982af41c32e140c
Minh <4501104141@student.hcmue.edu.vn>
2024-03-29 11:22:43 +0700

    unittest_user

--- src/KnowledgeSpace.ViewModels/Systems/UserCreateRequestValidator.cs
+++ src/KnowledgeSpace.ViewModels/Systems/UserCreateRequestValidator.cs
@@ -1,7 +1,4 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace KnowledgeSpace.ViewModels.Systems
 {
@@ -10,7 +7,10 @@ namespace KnowledgeSpace.ViewModels.Systems
         public UserCreateRequestValidator()
         {
             RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required");
-            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required")
+                .MinimumLength(8).WithMessage("Password has to atleast 8 characters")
+                .Matches(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+                .WithMessage("Password is not match complexity rules.");
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required")
                 .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email format is not match");
             RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number is required");
